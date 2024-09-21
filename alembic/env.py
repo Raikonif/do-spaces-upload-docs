d07b17818709761692b6f927fae2db00ddd42c17
Raikonif <raikonif@gmail.com>
2024-09-20 20:59:11 -0400

    implemented alembic migrations and models modification

--- alembic/env.py
+++ alembic/env.py
@@ -7,10 +7,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.database import Base
+from app.models import FileDO
+
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL")
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,8 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
-
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
