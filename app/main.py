879b3f2650e00d3ed52bb39b92cb385f2ca76771
Raikonif <raikonif@gmail.com>
2024-09-24 10:05:01 -0400

    https middleware added

--- app/main.py
+++ app/main.py
@@ -32,10 +32,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_middleware(HTTPSRedirectMiddleware)
 
+# app.add_middleware(HTTPSRedirectMiddleware)
 
 Base.metadata.create_all(bind=engine)
+
 
 @app.get("/test")
 async def test_route():
